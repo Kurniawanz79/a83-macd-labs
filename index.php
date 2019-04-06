db035fb3a9adc118fd7c9df7e139b43ca30e4cb1
Kurniawanz79 <49310559+Kurniawanz79@users.noreply.github.com>
2019-04-06 10:33:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<kurniawanzcasperwebapps.database.windows.net>";
-    $user = "<kurniawanz>";
-    $pass = "<Casper1979>";
-    $db = "<kurniawanzcasperwebappdb>";
+    $host = "kurniawanzcasperwebapps.database.windows.net";
+    $user = "kurniawanz";
+    $pass = "Casper1979";
+    $db = "kurniawanzcasperwebappdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
